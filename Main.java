d531b43250516ed4030b04dfbc3e5e87175faadb
prasad_9689 <84795268+PrasadShirude@users.noreply.github.com>
2021-06-25 00:15:17 +0530

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,17 @@
+import javax.swing.*;
+import java.awt.*;
+
+class Main
+{
+	public static void main(String args[])
+	{
+		JFrame fobj = new JFrame("File Packer and Unpacker");
+
+		fobj.setColor("Blue");
+		fobj.setSize(1000,700);
+
+		
+
+		fobj.setVisible(true);
+	}
+}
